ed1b81b7811ee0164b1062edd20fb3bbbc10e326
miyagawa <2101297@s.asojuku.ac.jp>
2023-05-17 09:59:34 +0900

    table作成仮完了

--- backend/sql/DB.sql
+++ backend/sql/DB.sql
@@ -2,7 +2,7 @@
 CREATE TABLE user_tbl(
     user_id         INT             AUTO_INCREMENT  ,
     user_pass       VARCHAR(255)    NOT NULL        ,
-    user_mail       VARCHAR(50)     NOT NULL        ,
+    user_mail       VARCHAR(100)    NOT NULL        ,
     PRIMARY KEY     (user_id)
 );
 
@@ -16,5 +16,26 @@ CREATE TABLE gpt_tbl(
     position        VARCHAR(50)     NOT NULL        ,
     search          INT             DEFAULT 0       ,
     PRIMARY KEY     (gpt_id)                        ,
-    FOREIGN KEY     (user_id) REFERENCES user_tbl(user_id)
+    FOREIGN KEY     (user_id)       REFERENCES user_tbl(user_id)
+);
+
+//ディベートテーブル
+CREATE TABLE debate_tbl(
+    debate_id       INT             AUTO_INCREMENT  ,
+    user_id         INT                             ,
+    title           VARCHAR(100)    NOT NULL        ,
+    judgement       VARCHAR(30)                     ,
+    PRIMARY KEY     (debate_id)                     ,
+    FOREIGN KEY     (user_id)       REFERENCES user_tbl(user_id)
+);
+
+//トークテーブル
+CREATE TABLE talk_tbl(
+    talk_id         INT             AUTO_INCREMENT  ,
+    debate_id       INT                             ,
+    gpt_id          INT                             ,
+    talk            VARCHAR(1000)   NOT NULL        ,
+    PRIMARY KEY     (talk_id)                       ,
+    FOREIGN KEY     (debate_id)     REFERENCES debate_tbl(debate_id),
+    FOREIGN KEY     (gpt_id)        REFERENCES gpt_tbl(gpt_id)
 );
