3eed3f7ececa32380ec2c5480dbf2e1d2ef492f7
kanapiii <113953054+kanapiii@users.noreply.github.com>
2023-05-17 21:19:06 +0900

    ログ表示画面UI完成
    確認お願いします

---
+++ frontend/src/components/debate_rog/rogari1.tsx
@@ -0,0 +1,13 @@
+import style from "../../styles/Rog/Rogari1.module.css";
+
+const Rogari1 = () =>{
+  return(
+    <div className={style.rogari_area}>
+      <img src="../../../public/image/gpt.png" alt="" className={style.img1}/>
+      <div className={style.fukidasi_area}>
+      <p className={style.p1}>ああああああああああああああああああああああああああああああああああああああああああああ</p>
+      </div>
+    </div>
+  )
+}
+export default Rogari1;
---
+++ frontend/src/components/debate_rog/tozirubtn.tsx
@@ -0,0 +1,10 @@
+import style from "../../styles/Rog/Tozirubtn.module.css";
+
+const Tozirubtn = () =>{
+  return(
+    <div className={style.img}>
+      <p className={style.maru}>×</p>
+    </div>
+  )
+}
+export default Tozirubtn;
