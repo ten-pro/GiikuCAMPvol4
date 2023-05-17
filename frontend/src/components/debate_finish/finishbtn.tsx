536ead80790ec398df9c7d51dc86e18a65a08a6c
kanapiii <113953054+kanapiii@users.noreply.github.com>
2023-05-17 12:07:35 +0900

    debate_finish画面UI完成
    修正ポイントの指示お願いします

---
+++ frontend/src/components/debate_finish/finishbtn.tsx
@@ -0,0 +1,12 @@
+import style from "../../styles/Debate_finish/Finishbtn.module.css";
+
+const Finishbtn = () =>{
+  return(
+    <>
+    <div className={style.btn_area}>
+      <p className={style.p}>ジャッジへ</p>
+    </div>
+    </>
+  )
+}
+export default Finishbtn;
