189ea79c8716bc36743efdb3736dc5187baf7dcc
明石凜太 <jeffrey.bezos@icloud.com>
2023-05-17 12:09:35 +0900

    再調整

--- frontend/src/components/Record/RecordLeft.tsx
+++ frontend/src/components/Record/RecordLeft.tsx
@@ -9,17 +9,19 @@ return (
 <>
     <div className={Styles.left}>
         <div className={Styles.buttonLine}>
-        <button className={Styles.homeButton}>く&nbsp;ホーム画面</button>
+            <button className={Styles.homeButton}>く&nbsp;ホーム画面</button>
         </div>
         <div className={Styles.leftBottom}>
-        <div className={Styles.title}>
-        <div className={Styles.titleText}>タイトル：</div>
-        <div className={Styles.titleVar}>必要なのは電気  OR  ガス</div>
-        </div>
-            <LeftContnts name='Ada' position='頭でっかち'/>
-            <LeftContnts name='Babbage' position='命知らず'/>
-            <LeftContnts name='Curie' position='正直者'/>
-            <LeftContnts name='Davinci' position='力自慢'/>
+            <div className={Styles.leftBottomm}>
+                <div className={Styles.title}>
+                    <div className={Styles.titleText}>タイトル：</div>
+                    <div className={Styles.titleVar}>必要なのは電気 OR ガス</div>
+                </div>
+                <LeftContnts name='Ada' position='頭でっかち' />
+                <LeftContnts name='Babbage' position='命知らず' />
+                <LeftContnts name='Curie' position='正直者' />
+                <LeftContnts name='Davinci' position='力自慢' />
+            </div>
         </div>
     </div>
 </>
