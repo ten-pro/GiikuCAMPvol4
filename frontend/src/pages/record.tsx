61eda9633a7291ccd0e89691c1bb4298e8c729bf
明石凜太 <jeffrey.bezos@icloud.com>
2023-05-20 18:24:51 +0900

    API接続完了201824

--- frontend/src/pages/record.tsx
+++ frontend/src/pages/record.tsx
@@ -1,16 +1,38 @@
 import Left from '@/components/Record/RecordLeft'
 import Right from '@/components/Record/RecordRight'
 import Styles from '@/styles/Record/Record.module.scss'
+import axios from 'axios'
 import { Inter } from 'next/font/google'
-
 const inter = Inter({ subsets: ['latin'] })
 
 function Home() {
+    const get_record = async () => {
+        try {
+            const res = await axios.post(
+                'https://mp-class.chips.jp/debate/Main.php',
+                {
+                    get_minutes: '',
+                    user_id: 1
+                },
+                {
+                    headers: {
+                        'Content-Type': 'multipart/form-data',
+                    },
+                }
+            );
+            console.log(res.data);
+            console.log(res.data.length);
+        } catch(error) {
+            console.log(error);
+        }
+    };
+
 return (
 <>
     <div className={Styles.record}>
         <Left />
         <Right />
+        <button onClick={ get_record }>get</button>
     </div>
 </>
 )
