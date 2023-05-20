4c9009ebc354ada79039aaa981fb191d8e4c139e
miyagawa <2101297@s.asojuku.ac.jp>
2023-05-20 10:56:37 +0900

    DebateAPIの作成

--- backend/api/Main.php
+++ backend/api/Main.php
@@ -7,19 +7,58 @@ header('Content-Type: application/json; charset=UTF-8');
 
 //DAOの読み込み
 require_once './UserDAO.php';
+require_once './GptDAO.php';
+require_once './DebateDAO.php';
 
-//ログイン処理
+$data = 'noapi';
 
 //login_userの引数がある時の処理
 if (isset($_POST['login_user']) == true) {
     $class = new Login();
-        $data = $class->login_user($_POST['mail'], $_POST['pass']);
+    $data = $class->login_user($_POST['mail'], $_POST['pass']);
 }
 
 //create_userの引数がある時の処理
 if (isset($_POST['create_user']) == true) {
     $class = new Login();
-        $data = $class->create_user($_POST['pass'], $_POST['mail']);
+    $data = $class->create_user($_POST['pass'], $_POST['mail']);
+}
+
+$json_data = json_decode(file_get_contents('php://input'), true);
+
+//create_gptの引数がある時の処理
+if (isset($json_data['create_gpt']) && !empty($json_data['gpts'])) {
+    $class = new Gpt();
+    $data = $class->create_gpt($json_data['gpts']);
+}
+
+//create_talkの引数がある時の処理
+if (isset($json_data['create_talk']) && !empty($json_data['talks'])) {
+    $class = new Debate();
+    $data = $class->create_talk($json_data['talks']);
+}
+
+//create_debateの引数がある時の処理
+if (isset($json_data['create_debate']) && !empty($json_data['gpts']) && !empty($json_data['debate'])) {
+    $class1 = new Gpt();
+    $class2 = new Debate();
+    $data =
+        array(
+            'gpts_id' => $class1->create_gpt($json_data['gpts']),
+            'debate_id' => $class2->create_debate($json_data['debate'])
+        );
+}
+
+//get_gptlistの引数がある時の処理
+if (isset($_POST['get_gptlist']) == true) {
+    $class = new Gpt();
+    $data = $class->get_gptlist($_POST['id']);
+}
+
+//search_gptの引数がある時の処理
+if (isset($_POST['search_gpt']) == true) {
+    $class = new Gpt();
+    $data = $class->search_gpt($_POST['character']);
 }
 
 
